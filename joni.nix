459dece7360deb831dcb253ae33e541dd34ec56c
jsz-nine <jsz@nine.dk>
2025-07-09 20:10:03 +0200

    second commit

---
+++ joni.nix
@@ -0,0 +1,46 @@
+{ config, lib, pkgs, ... }: {
+
+  imports = [
+    ./zsh.nix
+  ];
+
+  home.file = lib.mkMerge [{
+    "./.gitconfig".source = ./configs/gitconfig;
+    ".powerlevel10k".source = ./configs/p10k.zsh;
+  }];
+
+  home.packages = with pkgs; [
+    neovim
+    git
+    htop
+    zsh-powerlevel10k
+    oh-my-zsh
+  ];
+  programs.git.enable = true;
+  programs.zoxide.enable = true;
+  programs.zoxide.enableZshIntegration = true;
+  programs.helix = {
+    enable = true;
+    settings = {
+      theme = "autumn_night_transparent";
+      editor.cursor-shape = {
+        normal = "block";
+        insert = "bar";
+        select = "underline";
+      };
+    };
+    languages.language = [{
+      name = "nix";
+      auto-format = true;
+      formatter.command = "${pkgs.nixfmt-classic}/bin/nixfmt";
+    }];
+    themes = {
+      autumn_night_transparent = {
+        "inherits" = "autumn_night";
+        "ui.background" = { };
+      };
+    };
+  };
+
+  home.stateVersion = "24.05";
+}
